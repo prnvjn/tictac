32b66ca4bc4e1e40b7b834f84f9bb74cdc0602e7
Pranav <prnvjn@gmail.com>
2021-09-04 23:26:20 +0530

    able to switch between x and o

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const players = (player, mark)=>{
     return{player,mark}
 }
 
-const player1 = players('playerX','X')
-const player2 = players('playerO','O')
-
+const player1 = players('playerX','❌')
+const player2 = players('playerO','⭕️')
+let defaultMarkX = true
+console.log(defaultMarkX)
 let container = document.querySelector('.container')
+valueArr =[]
 
 
 let doSomething = (e) =>{
    
-    e.target.innerHTML = player1.mark
-        console.log(e.target)
+    e.target.innerHTML = defaultMarkX? player1.mark: player2.mark
+    defaultMarkX=(!defaultMarkX)
+    console.log(e.target)
     
 }
 
@@ -20,8 +23,12 @@ container.addEventListener('click',doSomething)
 
 let board = document.querySelectorAll('.board')
 board = Array.from(board)
-console.log(board)
-board.forEach( (item)=> 
-    l
-    item.innerHTML = player1.mark
-)
+console.log(board) //on click of the button add it to the array as well 
+
+board.forEach( (item)=> {
+    let value = item.innerHTML
+    valueArr.push(value)
+    console.log(valueArr)
+    
+})
+
